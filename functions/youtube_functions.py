082be64948ee37a89b1e1d491f4e3bf27cde5973
Gamer2020 <lostatlife2020@hotmail.com>
2023-04-16 17:36:51 -0400

    get youtube video info

--- functions/youtube_functions.py
+++ functions/youtube_functions.py
@@ -1,3 +1,4 @@
+import re
 import requests
 
 
@@ -32,3 +33,39 @@ def get_video_urls_from_channel_list(api_key, channel_ids):
                 break
 
     return video_urls
+
+
+def get_video_details(api_key, video_url):
+    video_id_regex = re.compile(r"v=([^&]+)")
+    match = video_id_regex.search(video_url)
+
+    if not match:
+        return None
+
+    video_id = match.group(1)
+
+    base_url = "https://www.googleapis.com/youtube/v3/videos"
+    params = {
+        "key": api_key,
+        "id": video_id,
+        "part": "snippet",
+        "fields": "items(snippet(title,channelTitle,channelId,publishedAt,description))",
+    }
+
+    response = requests.get(base_url, params=params)
+    response_json = response.json()
+
+    if not response_json["items"]:
+        return None
+
+    snippet = response_json["items"][0]["snippet"]
+
+    video_details = {
+        "video_name": snippet["title"],
+        "channel_name": snippet["channelTitle"],
+        "channel_url": f"https://www.youtube.com/channel/{snippet['channelId']}",
+        "publish_date": snippet["publishedAt"],
+        "video_description": snippet["description"],
+    }
+
+    return video_details
--- youtube-channel-scrape.py
+++ youtube-channel-scrape.py
@@ -7,7 +7,7 @@ with open("config.yml") as config_file:
 
 # Access the value of the list in the configuration data
 
-print(config_data)
+# print(config_data)
 
 config_es_api_url = config_data["config"]["es_api_url"]
 config_es_api_version = config_data["config"]["es_api_version"]
@@ -17,6 +17,14 @@ config_youtube_api_key = config_data["config"]["youtube_api_key"]
 config_youtube_channels = config_data["config"]["youtube_channels"]
 
 video_urls = functions.youtube_functions.get_video_urls_from_channel_list(
-    config_youtube_api_key,config_youtube_channels
+    config_youtube_api_key, config_youtube_channels
 )
-print(video_urls)
+# print(video_urls)
+
+for video_url in video_urls:
+    video_info = functions.youtube_functions.get_video_details(
+        config_youtube_api_key, video_url
+    )
+    print(video_url)
+    print(video_info)
+    break
