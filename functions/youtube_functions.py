2dd8740ed397695875d7fb908ee8c2fc491c01af
Gamer2020 <lostatlife2020@hotmail.com>
2023-04-17 11:18:35 -0400

    Start to divide youtube scraping BC API Limits

--- functions/youtube_functions.py
+++ functions/youtube_functions.py
@@ -1,8 +1,31 @@
 import re
 import requests
-from bs4 import BeautifulSoup
 
-def get_video_urls_from_channel_list(api_key, channel_ids):
+
+def get_video_urls_from_channel_list_50(api_key, channel_ids):
+    video_urls = []
+
+    for channel_id in channel_ids:
+        base_url = "https://www.googleapis.com/youtube/v3/search"
+        params = {
+            "key": api_key,
+            "channelId": channel_id,
+            "part": "snippet",
+            "type": "video",
+            "maxResults": 50,  # Maximum allowed by the API
+            "fields": "items/id/videoId",
+        }
+
+        response = requests.get(base_url, params=params)
+        response_json = response.json()
+        for item in response_json["items"]:
+            video_url = "https://www.youtube.com/watch?v=" + item["id"]["videoId"]
+            video_urls.append(video_url)
+
+    return video_urls
+
+
+def get_video_urls_from_channel_list_full(api_key, channel_ids):
     video_urls = []
 
     for channel_id in channel_ids:
@@ -31,21 +54,6 @@ def get_video_urls_from_channel_list(api_key, channel_ids):
             next_page_token = response_json.get("nextPageToken")
             if not next_page_token:
                 break
-
-    return video_urls
-
-def get_video_urls_from_channel_list2(channel_ids):
-    video_urls = []
-
-    for channel_id in channel_ids:
-        base_url = f"https://www.youtube.com/channel/{channel_id}/videos"
-
-        response = requests.get(base_url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        for video in soup.select("a[href*='/watch?v=']"):
-            video_url = "https://www.youtube.com" + video['href']
-            video_urls.append(video_url)
 
     return video_urls
 
---
+++ youtube-channel-scrape-50.py
@@ -0,0 +1,34 @@
+import functions.es_deck_functions
+import functions.youtube_functions
+import yaml
+
+# Read the configuration data from the file
+with open("config.yml") as config_file:
+    config_data = yaml.load(config_file, Loader=yaml.FullLoader)
+
+# Access the value of the list in the configuration data
+
+# print(config_data)
+
+config_es_api_base_url = config_data["config"]["es_api_url"]
+config_es_api_version = config_data["config"]["es_api_version"]
+config_es_api_full_url = config_es_api_base_url + config_es_api_version + "/"
+config_es_api_key = config_data["config"]["es_api_key"]
+config_youtube_api_key = config_data["config"]["youtube_api_key"]
+
+config_youtube_channels = config_data["config"]["youtube_channels_50"]
+
+video_urls = functions.youtube_functions.get_video_urls_from_channel_list_50(
+    config_youtube_api_key, config_youtube_channels
+)
+print(video_urls)
+
+for video_url in video_urls:
+    video_info = functions.youtube_functions.get_video_details(
+        config_youtube_api_key, video_url
+    )
+    print(video_url)
+    print(video_info)
+    if functions.es_deck_functions.youtube_check_deck_exists(video_url, config_es_api_full_url, config_es_api_key) == False:
+      functions.es_deck_functions.youtube_create_deck(video_info, video_url, config_es_api_full_url, config_es_api_key)
+    break
