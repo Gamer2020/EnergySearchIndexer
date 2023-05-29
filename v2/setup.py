a23a6a2707447e139eededfaeea14441cf0c3254
Gamer2020 <lostatlife2020@hotmail.com>
2023-05-29 16:03:17 -0400

    Start to generate table.

--- v2/setup.py
+++ v2/setup.py
@@ -9,31 +9,45 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(current_dir)
 
 # Open the file using the updated path
-config_path = os.path.join(current_dir, 'config.yml')
-with open(config_path, 'r') as config_file:
+config_path = os.path.join(current_dir, "config.yml")
+print("Config path:", config_path)
+
+with open(config_path, "r") as config_file:
     yaml_data = yaml.safe_load(config_file)
 
 # Get the preloaded_channels from the YAML data
-preloaded_channels = yaml_data['config']['preloaded_channels']
+preloaded_channels = yaml_data["config"]["preloaded_channels"]
+print("Preloaded channels:", preloaded_channels)
 
 # Connect to the database (creates a new file if it doesn't exist)
-conn = sqlite3.connect('youtube.db')
+conn = sqlite3.connect("youtube.sqlite")
+print("Connected to database")
 
 # Create a cursor object to execute SQL commands
 cursor = conn.cursor()
 
 # Create a table if it doesn't exist
-cursor.execute('''CREATE TABLE IF NOT EXISTS onboarded_channels (
+cursor.execute(
+    """CREATE TABLE IF NOT EXISTS onboarded_channels (
                     id TEXT PRIMARY KEY,
                     name TEXT NOT NULL
-                )''')
+                )"""
+)
+print("Table created")
 
 # Insert or update preloaded_channels in the database
 for channel in preloaded_channels:
-    channel_id = channel.split()[0].strip('"')
-    channel_name = channel.split()[1].strip('#@')
-    cursor.execute("INSERT OR REPLACE INTO onboarded_channels (id, name) VALUES (?, ?)", (channel_id, channel_name))
+    channel_parts = channel.split("|")
+    if len(channel_parts) >= 2:
+        channel_id = channel_parts[0].strip()
+        channel_name = channel_parts[1].strip()
+        print("Inserting channel:", channel_id, channel_name)
+        cursor.execute(
+            "INSERT OR REPLACE INTO onboarded_channels (id, name) VALUES (?, ?)",
+            (channel_id, channel_name),
+        )
 
 # Commit the changes and close the connection
 conn.commit()
 conn.close()
+print("Database updated successfully")
